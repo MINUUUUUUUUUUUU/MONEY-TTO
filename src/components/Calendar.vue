885bbced99885ba98fc97e5869708b8e3cb3afa0
guddnboy <hung3018@naver.com>
2025-04-08 21:36:09 +0900

    feat: 달력 내부에 수입, 지출 항목 표시

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -7,8 +7,28 @@ import FullCalendar from '@fullcalendar/vue3';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { useTradeListStore } from '@/stores/tradeList';
+import { computed, reactive, watch } from 'vue';
 
-const calendarOptions = {
+const tradeListStore = useTradeListStore();
+const tradeList = computed(() => tradeListStore.tradeList);
+
+const convertToCalendarEvents = (tradeList) => {
+  return tradeList.map((trade) => {
+    const isIncome = trade.tradeType === '수입';
+    return {
+      title: `${isIncome ? '+' : '-'}${trade.tradeAmount.toLocaleString()}`,
+      start: trade.tradeDate,
+      color: isIncome ? 'green' : 'red',
+      extendedProps: {
+        description: trade.tradeDescription,
+        category: trade.incomeCategory,
+      },
+    };
+  });
+};
+
+const calendarOptions = reactive({
   plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
   initialView: 'dayGridMonth',
   headerToolbar: {
@@ -16,10 +36,22 @@ const calendarOptions = {
     center: 'title',
     right: 'next',
   },
-  themeSystem: 'bootstrap',
+  contentHeight: 'auto',
   selectable: true,
+  editable: true,
   weekends: true,
+  displayEventTime: false,
+  firstDay: 0,
   locale: 'ko',
-  select: () => {},
-};
+  events: [],
+});
+
+// tradeList를 감시하면서 캘린더에 넣을 이벤트로 변환
+watch(
+  tradeList,
+  (newVal) => {
+    calendarOptions.events = convertToCalendarEvents(newVal);
+  },
+  { immediate: true }
+);
 </script>
