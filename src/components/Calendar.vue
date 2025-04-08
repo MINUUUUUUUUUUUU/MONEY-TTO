1071d5d31ef8402735ead2b2e7a74e15434462b8
guddnboy <hung3018@naver.com>
2025-04-08 16:23:33 +0900

    refactor: 캘린더 컴포넌트 분리

---
+++ src/components/Calendar.vue
@@ -0,0 +1,25 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script setup>
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const calendarOptions = {
+  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev',
+    center: 'title',
+    right: 'next',
+  },
+  themeSystem: 'bootstrap',
+  selectable: true,
+  weekends: true,
+  locale: 'ko',
+  select: () => {},
+};
+</script>
