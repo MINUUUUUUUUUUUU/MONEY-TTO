544fbf4d6f0c96e00849baf865de82ed68a754bc
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2025-04-09 10:23:44 +0900

    feat: alert component 구현, show 메소드 구현

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,29 @@
     <!-- 헤더 컴포넌트를 표시 -->
     <Header />
 
+    <Alert v-if="alertMessage" :message="alertMessage" :type="alertType" />
+    <button @click="triggerAlert" class="btn btn-success">성공 메시지</button>
+
     <!-- 라우터에 의해 매칭되는 컴포넌트를 표시하는 영역 -->
     <!-- 클릭된 RouterLink에 따라 표시되는 컴포넌트가 변경됨 -->
     <RouterView />
-
-
   </div>
 </template>
 
 <script setup>
+import Alert from './components/Alert.vue';
 import Header from './components/Header.vue';
+
+import { ref } from 'vue';
+
+const alertMessage = ref('');
+const alertType = ref('info');
+
+const triggerAlert = () => {
+  alertMessage.value = ''; // 리셋
+  setTimeout(() => {
+    alertMessage.value = '성공이여~';
+    alertType.value = 'success';
+  }, 300); // 아주 짧은 지연 후 다시 설정
+};
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -11,56 +11,56 @@ const router = createRouter({
       component: () => import('../pages/Home.vue'),
     },
 
-    // 로그인, 회원가입
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../pages/Login.vue'),
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../pages/Register.vue'),
-    },
+    // // 로그인, 회원가입
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: () => import('../pages/Login.vue'),
+    // },
+    // {
+    //   path: '/register',
+    //   name: 'register',
+    //   component: () => import('../pages/Register.vue'),
+    // },
 
-    // 거래 추가 페이지
-    {
-      path: '/trade/add',
-      name: 'addTrade',
-      component: () => import('../pages/AddTrade.vue'),
-    },
-    // 거래 리스트 페이지
-    {
-      path: '/trade',
-      name: 'tradeList',
-      component: () => import('../pages/TradeList.vue'),
-    },
-    // 거래 상세 페이지
-    {
-      path: '/trade/:tradeId',
-      name: 'tradeDetail',
-      component: () => import('../pages/TradeDetail.vue'),
-    },
-    // 거래 수정 페이지
-    {
-      path: '/trade/:tradeId/edit',
-      name: 'editTrade',
-      component: () => import('../pages/EditTrade.vue'),
-    },
+    // // 거래 추가 페이지
+    // {
+    //   path: '/trade/add',
+    //   name: 'addTrade',
+    //   component: () => import('../pages/AddTrade.vue'),
+    // },
+    // // 거래 리스트 페이지
+    // {
+    //   path: '/trade',
+    //   name: 'tradeList',
+    //   component: () => import('../pages/TradeList.vue'),
+    // },
+    // // 거래 상세 페이지
+    // {
+    //   path: '/trade/:tradeId',
+    //   name: 'tradeDetail',
+    //   component: () => import('../pages/TradeDetail.vue'),
+    // },
+    // // 거래 수정 페이지
+    // {
+    //   path: '/trade/:tradeId/edit',
+    //   name: 'editTrade',
+    //   component: () => import('../pages/EditTrade.vue'),
+    // },
 
-    // 사용자 정보 수정
-    {
-      path: '/profile/edit',
-      name: 'editProfile',
-      component: () => import('../pages/EditProfile.vue'),
-    },
+    // // 사용자 정보 수정
+    // {
+    //   path: '/profile/edit',
+    //   name: 'editProfile',
+    //   component: () => import('../pages/EditProfile.vue'),
+    // },
 
-    // Not Found
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'notFound',
-      component: () => import('../pages/NotFound.vue'),
-    },
+    // // Not Found
+    // {
+    //   path: '/:pathMatch(.*)*',
+    //   name: 'notFound',
+    //   component: () => import('../pages/NotFound.vue'),
+    // },
   ],
 });
 
