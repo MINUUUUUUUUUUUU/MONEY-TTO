e8881dae746b888ab818e9d0e5f5b6953140637c
syann97 <shallom75321@gmail.com>
2025-04-09 11:23:18 +0900

    feat: axios로 데이터 가져오기 #8

--- src/pages/TradeList.vue
+++ src/pages/TradeList.vue
@@ -2,94 +2,62 @@
   <div
     v-for="trade in tradeList"
     :key="trade.tradeId"
-    class="d-flex align-items-center me-3"
+    class="d-flex justify-content-between align-items-center py-2 border-bottom"
   >
-    <!-- 카테고리 + 아이콘 영역 -->
-    <div
-      class="d-flex align-items-center flex-shrink-0"
-      style="min-width: 100px; max-width: 150px"
-    >
-      <span class="fs-5 me-1">{{ trade.tradeCategory.split(' ')[0] }}</span>
-      <span class="fw-semibold text-nowrap">
-        {{ trade.tradeCategory.split(' ')[1] }}
-      </span>
+    <!-- 좌측: 이모지 + 내용 -->
+    <div class="d-flex align-items-center me-3 flex-grow-1">
+      <!-- 카테고리 + 아이콘 영역 -->
+      <div
+        class="d-flex align-items-center flex-shrink-0 fixed-category-width me-3"
+      >
+        <span class="fw-semibold text-nowrap">{{
+          trade.tradeType === '수입'
+            ? trade.incomeCategory
+            : trade.expenseCategory
+        }}</span>
+      </div>
+
+      <!-- 세부 내용 -->
+      <div class="overflow-hidden">
+        <div class="fw-semibold text-secondary text-truncate">
+          {{ trade.tradeDescription }}
+        </div>
+        <div class="text-muted small text-truncate">
+          {{ trade.tradeMethod }}
+        </div>
+      </div>
     </div>
 
-    <!-- 세부 내용 -->
-    <div class="overflow-hidden">
-      <div class="fw-semibold text-secondary text-truncate">
-        {{ trade.tradeDescription }}
-      </div>
-      <div class="text-muted small text-truncate">
-        {{ trade.tradeDate }} {{ trade.tradeMethod }}
+    <!-- 우측: 수입/지출 금액 -->
+    <div class="d-flex align-items-center justify-content-end">
+      <div
+        :class="[
+          'fw-littleBold',
+          trade.tradeType === '수입' ? 'text-success' : 'text-danger',
+        ]"
+      >
+        {{ trade.tradeType === '수입' ? '+' : '-' }}
+        {{ trade.tradeAmount.toLocaleString() }}원
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import axios from 'axios';
 import { ref } from 'vue';
+const tradeList = ref([]);
+const tradeUrlPrefix = '/api/tradeList/';
 
-const tradeList = ref([
-  {
-    tradeId: '1',
-    tradeType: '수입',
-    tradeAmount: 500000,
-    tradeCategory: '💰️ 급여',
-    tradeDate: '2025-04-01',
-    tradeMethod: '이체',
-    tradeDescription: '3월 급여 입금',
-    userId: '1',
-  },
-  {
-    tradeId: '2',
-    tradeType: '지출',
-    tradeAmount: 15000,
-    tradeCategory: '🍗 식비',
-    tradeDate: '2025-04-02',
-    tradeMethod: '카드',
-    tradeDescription: '점심 식사',
-    userId: '1',
-  },
-  {
-    tradeId: '3',
-    tradeType: '지출',
-    tradeAmount: 20000,
-    tradeCategory: '🚌교통비',
-    tradeDate: '2025-04-03',
-    tradeMethod: '현금',
-    tradeDescription: '택시비',
-    userId: '2',
-  },
-  {
-    tradeId: '4',
-    tradeType: '수입',
-    tradeAmount: 300000,
-    tradeCategory: '💰️ 급여',
-    tradeDate: '2025-04-01',
-    tradeMethod: '이체',
-    tradeDescription: '3월 급여 입금',
-    userId: '3',
-  },
-  {
-    tradeId: '5',
-    tradeType: '지출',
-    tradeAmount: 50000,
-    tradeCategory: '💰️ 세금',
-    tradeDate: '2025-04-04',
-    tradeMethod: '카드',
-    tradeDescription: '월세 납부',
-    userId: '4',
-  },
-  {
-    tradeId: '6',
-    tradeType: '지출',
-    tradeAmount: 12000,
-    tradeCategory: '🍗 식사',
-    tradeDate: '2025-04-05',
-    tradeMethod: '현금',
-    tradeDescription: '저녁 식사',
-    userId: '5',
-  },
-]);
+const fetchTradeList = async () => {
+  try {
+    const response = await axios.get(tradeUrlPrefix);
+    console.log(response.data);
+    tradeList.value = response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchTradeList();
 </script>
